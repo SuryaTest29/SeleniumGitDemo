86eb3ecddfe4268e19ea734daa6e22acbc981930
SuryaTest29 <surya.develop29@outlook.com>
2021-08-05 02:17:26 +0100

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	// All are just ummy cases
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test 
+	public void eelmentsUI()
+	{
+		System.out.println("eelmentsUI");
+	}
+}
